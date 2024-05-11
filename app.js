4ff465fea310f1267e9e3451002737db1525cabf
0adev <ayoubaguirar44@gmail.com>
2024-05-11 18:02:22 +0100

    Updated files

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const heroImages = [
 ];
 // main => benefits
 const benefitsImgs = document.querySelectorAll(".benefits img");
+const benefitsHeading = document.querySelector(".benefits h2");
+// main => process
+const processCards = [
+  document.querySelector(".first-step"),
+  document.querySelector(".second-step"),
+  document.querySelector(".third-step"),
+];
 
 const openMenu = () => {
   navList.classList.add("mobile-menu");
@@ -53,13 +60,20 @@ const imageAnimation = () => {
   const benefitsFourthImg = benefitsImgs[3];
   const benefitsFifthImg = benefitsImgs[4];
   const benefitsSixthImg = benefitsImgs[5];
+  console.log(window.scrollY);
 
-  const screenwidth = window.innerWidth; // Get the width of the viewport
+  const screenwidth = window.innerWidth;
   const largeScreenMin = 800;
   const largeScreenMax = 1524;
   const smallScreenMax = 796;
 
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
+  function benefitsHeadingAnimation(scrollThreshold) {
+    if (window.scrollY > scrollThreshold) {
+      benefitsHeading.classList.add("move-btt");
+    }
+  }
+
   function firstColumnsAnimation() {
     if (window.scrollY > 940) {
       benefitsFirstImg.classList.add("move-rtl");
@@ -119,6 +133,7 @@ const imageAnimation = () => {
 
   const largeScreens = () => {
     if (screenwidth >= largeScreenMin && screenwidth <= largeScreenMax) {
+      benefitsHeadingAnimation(680);
       firstColumnsAnimation();
       secondColumnsAnimation();
       lastColumnsAnimation();
@@ -127,6 +142,7 @@ const imageAnimation = () => {
 
   const smallScreens = () => {
     if (screenwidth <= smallScreenMax) {
+      benefitsHeadingAnimation(813);
       firstImageAnimation();
       secondImageAnimation();
       thirdImageAnimation();
@@ -140,6 +156,51 @@ const imageAnimation = () => {
   smallScreens();
 };
 
+const cardsAnimation = () => {
+  const screenwidth = window.innerWidth;
+  const largeScreenMin = 800;
+  const largeScreenMax = 1524;
+  const smallScreenMax = 796;
+
+  let scrollThreshold = window.scrollY;
+  console.log(window.scrollY);
+
+  function smallScreens() {
+    if (screenwidth <= smallScreenMax) {
+      if (scrollThreshold >= 4225) {
+        processCards[2].classList.add("scale-up");
+      } else if (scrollThreshold >= 3950) {
+        processCards[1].classList.add("scale-up");
+      } else if (scrollThreshold >= 3680) {
+        processCards[0].classList.add("scale-up");
+      } else {
+        processCards.forEach((card) => {
+          card.classList.remove("scale-up");
+        });
+      }
+    }
+  }
+
+  function largeScreens() {
+    if (screenwidth >= largeScreenMin && screenwidth <= largeScreenMax) {
+      if (scrollThreshold >= 3175) {
+        processCards[2].classList.add("scale-up");
+      } else if (scrollThreshold >= 2863) {
+        processCards[1].classList.add("scale-up");
+      } else if (scrollThreshold >= 2543) {
+        processCards[0].classList.add("scale-up");
+      } else {
+        processCards.forEach((card) => {
+          card.classList.remove("scale-up");
+        });
+      }
+    }
+  }
+
+  smallScreens();
+  largeScreens();
+};
+
 const init = () => {
   // Event listener for hamburger menu click
   hamburgerMenu.addEventListener("click", openMenu);
@@ -147,6 +208,8 @@ const init = () => {
   window.addEventListener("scroll", galleryScroll);
   // Event listener for benefits images
   window.addEventListener("scroll", imageAnimation);
+  // Event listener for process cards
+  window.addEventListener("scroll", cardsAnimation);
 };
 
 init();
